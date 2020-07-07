19ca14fe807b0609a7fbad140dc98d4b9abff89a
YuriyBelz <yuriybelz1@gmail.com>
2020-07-07 19:32:36 -0400

    testing .env, errors with oauth

--- server.js
+++ server.js
@@ -82,6 +82,8 @@ passport.deserializeUser(function(id, done) {
   });
 });
 
+
+console.log(process.env.GOOGLE_CLIENT_ID);
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
